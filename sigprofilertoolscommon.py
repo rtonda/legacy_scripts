06fa3cb66fbe8e4119ac0dc5645719ece2c35ab5
Raul Tonda <33624420+rtonda@users.noreply.github.com>
2024-01-22 13:14:56 +0100

    Add files via upload

---
+++ sigprofilertoolscommon.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+--------------------------------------------------------------------------------
+Bundle of functions common to more than one SigProfilerTools.
+--------------------------------------------------------------------------------
+"""
+
+def no_flags():
+    print('Please re-run the command with "-h" to get usage instructions and a\
+ complete list of options\n')
+    exit()
